b60b51545df171758fc1fe928b48c888f50371b4
Alireza Tajmirriahi <alireza.tajmirriahi@aalto.fi>
2024-04-04 11:05:25 +0300

    Add DataCleaning

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
+from scipy import stats
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.feature_selection import chi2
@@ -32,3 +33,35 @@ class Chi2(IFeatureExtractor):
         indices = np.argsort(chi2_stats)[::-1][:k]
         return X[:, indices]
 
+
+class DataCleaning:
+    @staticmethod
+    def remove_na(df, threshold):
+        return df.dropna(thresh=threshold, axis=1)
+    
+    def remove_duplicates(df):
+        return df.drop_duplicates()
+    
+    def remove_outliers(df):
+        return df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+    
+    def remove_low_variance(df, threshold):
+        return df.loc[:, df.var() > threshold]
+    
+    def remove_correlated_features(df, threshold):
+        corr_matrix = df.corr().abs()
+        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+        to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
+        return df.drop(to_drop, axis=1)
+    
+    def remove_low_importance_features(df, model, threshold):
+        model.fit(df)
+        importance = model.feature_importances_
+        return df.loc[:, importance > threshold]
+    
+    def clean_data(genera, metadata, mtb, na_threshold):
+        mtb = mtb.dropna(thresh=mtb.shape[0] * na_threshold, axis=1)
+        mtb = mtb.dropna(thresh=mtb.shape[1] * na_threshold, axis=0)
+        metadata = metadata[metadata['Sample'].isin(mtb.Sample)]
+        genera = genera[genera['Sample'].isin(mtb.Sample)]
+        return genera, metadata, mtb
