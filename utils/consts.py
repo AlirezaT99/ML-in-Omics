290734d1b6fb8640e8ce9f267bba98c8899e593b
Alireza Tajmirriahi <alireza.tajmirriahi@aalto.fi>
2024-04-04 10:04:37 +0300

    Update config file

--- utils/consts.py
+++ utils/consts.py
@@ -1,6 +1,15 @@
+from ..analysis.preprocessing import FeatureExtraction
+from ..analysis.models import Models
+
 DEFAULT_DATA_PATH = 'data/'
+OUTPUT_PATH = 'output/'
+OUTPUT_LOG_FILE = 'log.txt'
 DEFAULT_DEBUG_MODE = False
+
 MTB_NA_THRESHOLD = 0.90
+RANDOM_STATE = 42
+KFOLD_SPLITS = 5
+DEFAULT_SCORING = 'f1_weighted'
 
 ANALYSIS_PARAMS = {
     'mars_ibs_lr': {
